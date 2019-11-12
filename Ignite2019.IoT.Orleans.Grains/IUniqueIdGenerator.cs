77bb70b70f41bf1a3e9db83c163ff6839ee5fdde
Shengjie Yan <ysjshengjie@live.cn>
2019-11-12 22:27:21 +0800

    add grains project

---
+++ Ignite2019.IoT.Orleans.Grains/IUniqueIdGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Orleans;
+
+namespace Ignite2019.IoT.Orleans.Model.Grains
+{
+    /// <summary>
+    /// 唯一Id生成器
+    /// </summary>
+    public interface IUniqueIdGenerator:IGrainWithStringKey
+    {
+        Task<string> NewId();
+    }
+
+    public class GuidGenerator:Grain,IUniqueIdGenerator
+    {
+        public Task<string> NewId()
+        {
+            return Task.FromResult(Guid.NewGuid().ToString());
+        }
+    }
+
+    public class DeviceUniqueIdGenerator:Grain<Segment>,IUniqueIdGenerator
+    {
+        public Task<string> NewId()
+        {
+            var companyId = this.GetPrimaryKey();
+            // get assigned segments of company.
+            
+
+            var newId = Guid.NewGuid().ToString();
+
+            return Task.FromResult(newId);
+        }
+    }
+
+    public class Segment
+    {
+        public ulong LatestNum { get; set; }
+    }
+}
