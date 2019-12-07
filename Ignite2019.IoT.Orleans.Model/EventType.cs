c0609b2e08ab699ba0d21d404beb78e7f3e274d1
Shengjie Yan <ysjshengjie@live.cn>
2019-12-07 11:20:27 +0800

    add display attribute

--- Ignite2019.IoT.Orleans.Model/EventType.cs
+++ Ignite2019.IoT.Orleans.Model/EventType.cs
@@ -1,10 +1,16 @@
-﻿namespace Ignite2019.IoT.Orleans.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ignite2019.IoT.Orleans.Model
 {
     public enum EventType
     {
+        [Display(Name = "上线")]
         Online,
+        [Display(Name = "下线")]
         Offline,
+        [Display(Name = "状态上报")]
         Report,
+        [Display(Name = "指令下发")]
         Control,
     }
 }
